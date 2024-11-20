8f35f786010cb5d819ad2c6c1f030d0f96cae6de
Miu Georgian-Fabian <georgian-fabian.miu@s.unibuc.ro>
2024-11-20 18:28:47 +0000

    adding second script

---
+++ ex3.1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+#  $1 - Nume de host (domeniu)
+#  $2 - Adresa IP
+#  $3 - Server DNS pentru interogare
+
+validateip() {
+    local hostname=$1
+    local ip=$2
+    local dns_server=$3
+
+    if [[ -z "$hostname" || -z "$ip" || -z "$dns_server" ]]; then
+        echo "Usage: validateip <hostname> <ip> <dns_server>"
+        return 1
+    fi
+
+   
+    resolved_ip=$(dig +short @$dns_server $hostname | grep -w "$ip")
+
+    if [[ -z "$resolved_ip" ]]; then
+        return 1 
+    else
+        return 0
+    fi
+}
+
+
+dns_server="8.8.8.8"
+
+
+cat /etc/hosts | while read -r line
+do
+    
+    if [[ -z "$line" || "$line" =~ ^# ]]; then
+        continue
+    fi
+
+    
+    ip=$(echo "$line" | awk '{print $1}')
+    domeniu=$(echo "$line" | awk '{print $2}')
+
+   
+    if ! validateip "$domeniu" "$ip" "$dns_server"; then
+        echo "Bogus IP for $domeniu in /etc/hosts!"
+    fi
+done
